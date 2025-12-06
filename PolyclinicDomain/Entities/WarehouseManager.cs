c64bff3b4dbd5ffed9c8f609c6d43ac74fbed6fb
John Mauris López Ramos <84256793+John31415@users.noreply.github.com>
2025-12-06 18:02:40 -0500

    bugfix: delete warehouse entity (#60)

--- PolyclinicDomain/Entities/WarehouseManager.cs
+++ PolyclinicDomain/Entities/WarehouseManager.cs
@@ -5,23 +5,12 @@ namespace PolyclinicDomain.Entities;
 
 public class WarehouseManager : Employee
 {
-    public Guid WarehouseId { get; private set; }
-    public virtual Warehouse? Warehouse { get; private set; }
-
     public virtual ICollection<WarehouseRequest> WarehouseRequests { get; private set; } = new List<WarehouseRequest>();
 
-    public WarehouseManager(Guid id, string identification, string name, string employmentStatus, Guid warehouseId)
-        : base(id, identification, name, employmentStatus)
-    {
-        WarehouseId = warehouseId;
-    }
+    public WarehouseManager(Guid id, string identification, string name, string employmentStatus)
+        : base(id, identification, name, employmentStatus) {}
 
     protected WarehouseManager() { }
 
     public override string GetPrimaryRole() => ApplicationRoles.WarehouseManager;
-
-    public void AssignWarehouse(Guid warehouseId)
-    {
-        WarehouseId = warehouseId;
-    }
 }
