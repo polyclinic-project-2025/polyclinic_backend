aaf94ec3fe3dc3090b716e532700f030a83d0d84
ChertaPro <chertapro@gmail.com>
2025-12-07 17:59:56 -0500

    feat:add CRUD Operations for StockMedication

---
+++ PolyclinicDomain/IRepositories/IStockDepartmentRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PolyclinicDomain.Entities;
+using PolyclinicDomain.IRepositories;
+
+
+namespace PolyclinicDomain.IRepositories
+{
+    public interface IStockDepartmentRepository : IRepository<StockDepartment>
+    {
+        // Obtener todos los medicamentos y sus cantidades para un departamento
+        Task<IEnumerable<StockDepartment>> GetStockByDepartmentAsync(Guid departmentId);
+
+        // Obtener medicamentos cuyo stock está por debajo del mínimo
+        Task<IEnumerable<StockDepartment>> GetBelowMinQuantityAsync(Guid departmentId);
+
+        // Obtener medicamentos cuyo stock está por encima del máximo
+        Task<IEnumerable<StockDepartment>> GetAboveMaxQuantityAsync(Guid departmentId);
+    }
+}
---
+++ PolyclinicInfrastructure/Repositories/StockDepartmentRepository.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using PolyclinicDomain.Entities;
+using PolyclinicDomain.IRepositories;
+using PolyclinicInfrastructure.Persistence;
+
+
+namespace PolyclinicInfrastructure.Repositories
+{
+    public class StockDepartmentRepository : Repository<StockDepartment>, IStockDepartmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public StockDepartmentRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<StockDepartment>> GetStockByDepartmentAsync(Guid departmentId)
+        {
+            return await _context.StockDepartments
+                .Where(sd => sd.DepartmentId == departmentId)
+                .Include(sd => sd.Medication)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<StockDepartment>> GetBelowMinQuantityAsync(Guid departmentId)
+        {
+            return await _context.StockDepartments
+                .Where(sd => sd.DepartmentId == departmentId && sd.Quantity < sd.MinQuantity)
+                .Include(sd => sd.Medication)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<StockDepartment>> GetAboveMaxQuantityAsync(Guid departmentId)
+        {
+            return await _context.StockDepartments
+                .Where(sd => sd.DepartmentId == departmentId && sd.Quantity > sd.MaxQuantity)
+                .Include(sd => sd.Medication)
+                .ToListAsync();
+        }
+    }
+}
